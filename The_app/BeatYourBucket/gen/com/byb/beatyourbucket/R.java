33f3f863c5707b2f147b014a5ff9591e2b6af784
MarcSelles <mselles94@gmail.com>
2015-01-15 23:41:16 +0100

    list and fragments

--- The_app/BeatYourBucket/gen/com/byb/beatyourbucket/R.java
+++ The_app/BeatYourBucket/gen/com/byb/beatyourbucket/R.java
@@ -16,6 +16,9 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
+    public static final class array {
+        public static final int buckets=0x7f0c0000;
+    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1651,11 +1654,12 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050021;
         public static final int always=0x7f05000d;
         public static final int beginning=0x7f050012;
-        public static final int bucketlists=0x7f050047;
-        public static final int challenges=0x7f050049;
+        public static final int bucketlists=0x7f050043;
+        public static final int buckets=0x7f050040;
+        public static final int challenges=0x7f050045;
         public static final int checkbox=0x7f050029;
         public static final int collapseActionView=0x7f05000f;
-        public static final int completed=0x7f05004a;
+        public static final int completed=0x7f050046;
         public static final int decor_content_parent=0x7f05002f;
         public static final int default_activity_button=0x7f050024;
         public static final int dialog=0x7f050010;
@@ -1665,23 +1669,21 @@ containing a value of this type.
         public static final int end=0x7f050014;
         public static final int expand_activities_button=0x7f050022;
         public static final int expanded_menu=0x7f050028;
-        public static final int fraglists=0x7f05003f;
-        public static final int hoi=0x7f050044;
+        public static final int hoi=0x7f050041;
         public static final int home=0x7f050015;
         public static final int homeAsUp=0x7f050007;
         public static final int icon=0x7f050026;
         public static final int ifRoom=0x7f05000c;
         public static final int image=0x7f050023;
-        public static final int list=0x7f050042;
         public static final int listMode=0x7f050002;
         public static final int list_item=0x7f050025;
         public static final int middle=0x7f050013;
         public static final int never=0x7f05000b;
         public static final int none=0x7f050004;
         public static final int normal=0x7f050001;
-        public static final int positions=0x7f05004b;
-        public static final int profiel=0x7f050046;
-        public static final int profile=0x7f050048;
+        public static final int positions=0x7f050047;
+        public static final int profiel=0x7f050042;
+        public static final int profile=0x7f050044;
         public static final int progress_circular=0x7f05001c;
         public static final int progress_horizontal=0x7f05001d;
         public static final int radio=0x7f05002b;
@@ -1701,11 +1703,8 @@ containing a value of this type.
         public static final int showTitle=0x7f050008;
         public static final int split_action_bar=0x7f050019;
         public static final int submit_area=0x7f05003c;
-        public static final int tab1=0x7f050041;
-        public static final int tab2=0x7f050043;
-        public static final int tab3=0x7f050045;
         public static final int tabMode=0x7f050003;
-        public static final int tabhost=0x7f050040;
+        public static final int textView1=0x7f05003f;
         public static final int title=0x7f050027;
         public static final int up=0x7f050016;
         public static final int useLogo=0x7f050005;
@@ -1762,11 +1761,16 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int fragment_lists=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int tab1_view=0x7f03001a;
+        public static final int tab2_view=0x7f03001b;
+        public static final int tab3_view=0x7f03001c;
+        public static final int view_buckets=0x7f03001d;
     }
     public static final class menu {
-        public static final int list=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int list=0x7f0d0000;
+        public static final int main=0x7f0d0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
---
+++ The_app/BeatYourBucket/src/com/byb/beatyourbucket/FragmentProfile.java
@@ -0,0 +1,18 @@
+package com.byb.beatyourbucket;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentProfile  extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View v = inflater.inflate(R.layout.tab3_view, container, false);
+		return v;
+	}
+}
