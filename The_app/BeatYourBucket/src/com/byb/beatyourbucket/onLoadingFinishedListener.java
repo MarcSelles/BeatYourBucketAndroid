7bf9bcea8d2d92b4d43eb29193d03b148ed9868d
MarcSelles <mselles94@gmail.com>
2015-01-29 18:35:06 +0100

    Update onLoadingFinishedListener.java

--- The_app/BeatYourBucket/src/com/byb/beatyourbucket/onLoadingFinishedListener.java
+++ The_app/BeatYourBucket/src/com/byb/beatyourbucket/onLoadingFinishedListener.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.json.JSONObject;
 
+/* Ensures that the data will return only when completed*/
 public interface onLoadingFinishedListener {
 	public void onLoadingFinished(ArrayList<JSONObject> list);
 }
